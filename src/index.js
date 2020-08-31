188fa32da4d1b515f3e9d4b3cac8f5ff73125518
rsharpe23 <roman.sharpe23@gmail.com>
2020-08-31 17:44:50 +0300

    Updated project files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
-// TODO: Сделать деплой на github
-ReactDOM.render(<App />, document.getElementById('root'));
